e6637965b7879b854fac4d5092d8b2d64560175d
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-06-27 12:16:01 +0530

    auth api implemented

--- src/config/env.js
+++ src/config/env.js
@@ -6,7 +6,7 @@ const env = {
   PORT: process.env.PORT || 3000,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+  GOOGLE_CALLBACK_URL: process.env.NODE_ENV === "production" ? process.env.GOOGLE_CALLBACK_URL : "/api/auth/google/callback",
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV || "development",
   BCRYPT_SALT: Number(process.env.BCRYPT_SALT) ,
