df6dfb4e52680ecb69f2e6f876dcdc1899c43939
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-06-29 14:00:56 +0530

    Implemented and setup payment gateway

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: "same",
+      sameSite: env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
       secure: env.NODE_ENV === "production",
